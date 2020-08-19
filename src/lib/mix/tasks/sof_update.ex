8912d38eaa6d39c60edb2a5d21f9c713f9459c46
dingpl716 <dingpl716@gmail.com>
2020-08-19 16:44:55 -0700

    Update for 2020-08-18

--- src/lib/mix/tasks/sof_update.ex
+++ src/lib/mix/tasks/sof_update.ex
@@ -10,30 +10,39 @@ defmodule Mix.Tasks.Sof.Update do
 
   @shortdoc "Copy, update data and regenerate the index.html."
 
-  def run([mc_file, bwl_file]) do
+  def run([mc_file, bwl_file, taq_file]) do
     date = get_tuesday()
-    run(["--date", date, mc_file, bwl_file])
+    run(["--date", date, mc_file, bwl_file, taq_file])
   end
 
   def run(["--date", date, "--mc", mc_file]) do
-    File.cp!(mc_file, "../data/mc/#{date}.csv")
-    Sof.update_db("mc", "#{date}.csv")
-    Sof.refresh_html("mc")
+    do_run(date, "mc", mc_file)
   end
 
   def run(["--date", date, "--bwl", bwl_file]) do
-    File.cp!(bwl_file, "../data/bwl/#{date}.csv")
-    Sof.update_db("bwl", "#{date}.csv")
-    Sof.refresh_html("bwl")
+    do_run(date, "bwl", bwl_file)
   end
 
-  def run(["--date", date, mc_file, bwl_file]) do
+  def run(["--date", date, "--taq", taq_file]) do
+    do_run(date, "taq", taq_file)
+  end
+
+  def run(["--date", date, mc_file, bwl_file, taq_file]) do
     File.cp!(mc_file, "../data/mc/#{date}.csv")
     File.cp!(bwl_file, "../data/bwl/#{date}.csv")
+    File.cp!(taq_file, "../data/taq/#{date}.csv")
     Sof.update_db("mc", "#{date}.csv")
     Sof.update_db("bwl", "#{date}.csv")
+    Sof.update_db("taq", "#{date}.csv")
     Sof.refresh_html("mc")
     Sof.refresh_html("bwl")
+    Sof.refresh_html("taq")
+  end
+
+  defp do_run(date, dungeon, data_file) do
+    File.cp!(data_file, "../data/#{dungeon}/#{date}.csv")
+    Sof.update_db("#{dungeon}", "#{date}.csv")
+    Sof.refresh_html("#{dungeon}")
   end
 
   def get_tuesday() do
